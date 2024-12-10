17f3663e23d853bc16a249d19392113170fc6ea7
Oscar Burgos <Oskyburgos81@gmail.com>
2024-12-09 20:25:08 -0500

    utils

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,8 @@
+// list of enums
+//they cannot be created indside a class
+
+enum TextSizes { small, medium, large }
+
+enum OrderStaur { processing, shipped, delivered }
+
+enum PaymentMethods { paypal, googlePlay, applePay, visa, masterCard, creditCard, paystack, razordPay, paytm }
---
+++ lib/utils/constants/string_image.dart
@@ -0,0 +1,9 @@
+class TImage {
+  //app logos
+  // static const String darkAppLogo = "assets/logos/";
+  // static const String lightAppLogo = "assets/logos/";
+  
+  // //social logos
+  // static const String google = "assets/logos/";
+  // static const String facebook = "assets/logos/";
+}
